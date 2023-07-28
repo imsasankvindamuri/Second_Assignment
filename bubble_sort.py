0915fe61244e1b64273fdf96ede9b6fa345f6216
Sai Sasank Vindamuri <imsasankvindamuri@gmail.com>
2023-07-28 18:05:21 +0530

    2

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,20 +1,13 @@
 num=int(input("Enter Number of Elements: "))
-list = []
+list1 = []
 for i in range(num):
     entry=int(input("Enter element: "))
-    list.append(entry)
-print(f"Unorganized list: {list}\n\n")
-for vals in range(len(list) - 1):
-    for i in range(len(list) - 1):
-        if list[i] > list[i + 1]:
-            temp = list[i + 1]
-            list[i + 1] = list[i]
-            list[i] = temp
-print(f"Organized List: {list}\n\n")
-
-
-# Notes
-"""
-For 15 Elements it is near instantaneous
-
-"""
+    list1.append(entry)
+print(f"Unorganized list: {list1}\n\n")
+for vals in range(len(list1) - 1):
+    for i in range(len(list1) - 1):
+        if list1[i] > list1[i + 1]:
+            temp = list1[i + 1]
+            list1[i + 1] = list1[i]
+            list1[i] = temp
+print(f"Organized List: {list1}\n\n")
